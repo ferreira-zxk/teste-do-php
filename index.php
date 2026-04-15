801c737333f80ca3e308ee671c3740ca268603c1
Davi <daviferreiralima2008@gmail.com>
2026-04-15 15:56:27 -0400

    segunda parte

--- index.php
+++ index.php
@@ -45,3 +45,30 @@ $texto = 'Ele comprou uma ' . $produto . 'no Joey\'s.';
 // Declaração de uma variável NULL
 $nulo = NULL;
 ?>
+
+
+<?php
+define ("NOMECLIENTE", "MARCOS DE MELO");
+$endereco       = 'Rua das Violetas, 320';
+$bairro         = 'Jd. Callegari';
+$estado         = 'SP';
+$cep            = '13.181-659';
+$idade          = 18;
+define ("RG", "1234567-x");
+$fone           = '(99)  8888-9287';
+?>
+
+<html>
+    <head>
+        <title>Trabalhos com Variáveis</title>
+        <meta http-equiv="content-Type" content="text/html; charset=uft=-8">
+</head>
+<body>
+    <h1>Informações de Contato</h1>
+    <p>Nome completo: <?php echo NOMECLIENTE; ?></p>
+    <p><?php echo "$bairro - $estado - CEP: $cep";?></p>
+    <p>Telefone: <?php echo $fone;?></p>
+    <h2>Documentos</h2>
+    <?php echo "<p>Idade: $idade | RG: ". RG. "</p>"?>
+</body>
+</html>
